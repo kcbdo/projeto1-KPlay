fd08829aa90ee6c35fc7b22c36f636825f467883
Kathleen Barbosa <kathleen_barbosa@discente.ufg.br>
2025-04-22 10:07:20 -0300

    Adiciona head.blade.php e atualiza welcome.blade.php

---
+++ resources/views/head.blade.php
@@ -0,0 +1,9 @@
+<head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="Esse é o meu primeiro software para assistir vídeos!"> <!--pequena descrição do meu site;-->
+        <meta name="keywords" content="vídeos,playlists,músicas"> <!--palavras que vão auxiliar para encontrar conteúdo do site; no entanto, atualmente a IA já consegue varrer os sites de forma eficiente e definir qual o conteúdo do site-->
+        <meta name="author" content="Kathleen Barbosa">
+        <title>Kplay</title>
+
+</head>
