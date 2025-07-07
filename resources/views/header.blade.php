5ac0424ab372623e0024e1d04059e9aa7ad34bbc
Kathleen <kathleencaroline159@outlook.com>
2025-07-07 17:17:43 -0300

    Arrumando estilização

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,16 +1,13 @@
 <nav class="bg-white shadow fixed top-0 left-0 right-0 z-50">
-  <div class="container mx-auto flex items-center justify-between px-4 py-3">
+  <div class="container mx-auto flex items-center justify-between px-4 py-2">
     <a href="/" class="kplay-logo">KPlay</a>
-    
+
     <ul id="menu" class="flex space-x-8 font-medium">
       <li><a href="#" class="nav-link">Usuários</a></li>
       <li><a href="{{ route('video.index') }}" class="nav-link">Vídeos</a></li>
       <li><a href="{{ route('playlists.index')}}" class="nav-link">Playlists</a></li>
       <li><a href="/categories" class="nav-link">Categorias</a></li>
       <li><a href="#" class="nav-link">Comentários</a></li>
-      
     </ul>
-  </div>
+  </div> 
 </nav>
-
-<div class="h-16"></div>
--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -1,30 +1,25 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="description" content="Esse é o meu primeiro software para assistir vídeos!"> <!--pequena descrição do meu site;-->
-        <meta name="keywords" content="vídeos,playlists,músicas"> <!--palavras que vão auxiliar para encontrar conteúdo do site; no entanto, atualmente a IA já consegue varrer os sites de forma eficiente e definir qual o conteúdo do site-->
-        <meta name="author" content="Kathleen Barbosa">
-        <title>Kplay</title>
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-        @include ('head')
-       
-    </head>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Esse é o meu primeiro software para assistir vídeos!">
+    <meta name="keywords" content="vídeos,playlists,músicas">
+    <meta name="author" content="Kathleen Barbosa">
+    <title>Kplay</title>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    @include('head')
+</head>
 
-    </body>
+<body> 
 
-    <div class="container">
-    
-        @include ('header')
-        
-        
-        <!--incluir h1 aqui com título-->
-        
-        @yield ('page-container')
-    </div>       
-        
-    </body>
+    @include('header')
+
+    <div class="container mx-auto pb-8"> 
+        @yield('page-container')
+    </div>
+
+</body>
 
 </html>
